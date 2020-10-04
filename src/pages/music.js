ce7ae72698381516f5da7ef8fcde68ca96905145
Steven-Klavins <stevenklavins94@gmail.com>
2020-10-04 02:41:34 +0100

    chore: added music page component

---
+++ src/pages/music.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+import "../pages/music.css"
+
+const Music = () => (
+  <Layout>
+    <h1>Music</h1>
+  </Layout>
+)
+
+export default Music
