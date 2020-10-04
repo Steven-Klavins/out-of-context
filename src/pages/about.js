f194d5d315f4520e8b60c2bff7dc6e71e7ded4cc
Steven-Klavins <stevenklavins94@gmail.com>
2020-10-04 02:39:11 +0100

    chore: added about page component

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "../pages/about.css"
+import Layout from "../components/layout"
+
+const About = () => (
+  <Layout>
+    <h1>About</h1>
+  </Layout>
+)
+
+export default About
