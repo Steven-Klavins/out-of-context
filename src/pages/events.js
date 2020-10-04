575170d29b3c2e0c118bb106486f6060ab148545
Steven-Klavins <stevenklavins94@gmail.com>
2020-10-04 02:40:24 +0100

    chore: added events page component

---
+++ src/pages/events.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+import "../pages/events.css"
+
+const Events = () => (
+  <Layout>
+    <h1>Events</h1>
+  </Layout>
+)
+
+export default Events
