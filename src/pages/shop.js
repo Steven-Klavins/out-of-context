ea80ac7935301ed6fb9fae62c7535165ac1f95de
Steven-Klavins <stevenklavins94@gmail.com>
2020-10-04 02:43:04 +0100

    chore: added shop page component

---
+++ src/pages/shop.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+import "../pages/shop.css"
+
+const Shop = () => (
+  <Layout>
+    <h1>Shop</h1>
+  </Layout>
+)
+
+export default Shop
