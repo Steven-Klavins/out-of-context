470e786bdab7b5b09f531139abef99be68c3d810
Steven-Klavins <stevenklavins94@gmail.com>
2020-10-04 02:42:08 +0100

    chore: added news page component

---
+++ src/pages/news.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+import "../pages/news.css"
+
+const News = () => (
+  <Layout>
+    <h1>News</h1>
+  </Layout>
+)
+
+export default News
