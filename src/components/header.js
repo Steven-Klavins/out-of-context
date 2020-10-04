e40652dba263bd5ffbad3be37e68887c78a38080
Steven-Klavins <stevenklavins94@gmail.com>
2020-10-04 02:37:53 +0100

    chore: updated navbar link locations

--- src/components/header.js
+++ src/components/header.js
@@ -1,26 +1,18 @@
-import { Link } from "gatsby"
 import React from "react"
-import { Navbar, Nav, NavDropdown } from "react-bootstrap"
+import { Navbar, Nav } from "react-bootstrap"
 import "../components/header.css"
 
 const Header = ({ siteTitle }) => (
   <Navbar bg="dark" expand="lg">
-    <Navbar.Brand href="#home">Out Of Context</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="mr-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#music">Music</Nav.Link>
-        <Nav.Link href="#news">News</Nav.Link>
-        <Nav.Link href="#events">Events</Nav.Link>
-        <Nav.Link href="#shop">Shop</Nav.Link>
-        <NavDropdown title="More" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-        </NavDropdown>
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="music">Music</Nav.Link>
+        <Nav.Link href="news">News</Nav.Link>
+        <Nav.Link href="events">Events</Nav.Link>
+        <Nav.Link href="shop">Shop</Nav.Link>
+        <Nav.Link href="about">About</Nav.Link>
       </Nav>
     </Navbar.Collapse>
   </Navbar>
