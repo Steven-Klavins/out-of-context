de8956f1f797e7ee3e03eb088ea57f7e05a54ab7
Steven-Klavins <stevenklavins94@gmail.com>
2020-10-07 22:13:53 +0100

    feat: configured wordpress source

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,8 +9,8 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `src`,
+        path: `${__dirname}/src/`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -24,11 +24,20 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-transformer-remark`,
+    },
+
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        baseUrl: `outofcontext257208211.wordpress.com/`,
+        protocol: `https`,
+        hostingWPCOM: true,
+      },
+    },
   ],
 }
