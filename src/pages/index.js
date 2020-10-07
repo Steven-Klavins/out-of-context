373e9081abeadcccbc7baebc2718ba333bf8f48b
Steven-Klavins <stevenklavins94@gmail.com>
2020-10-07 22:12:42 +0100

    chore: added markdown test for description

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,30 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import "../pages/index.css"
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Welcome</h1>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  console.log(data)
+  return (
+    <Layout>
+      <h1>Welcome</h1>
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+// Test: Markdown exsport passed to data prop from markdown folder (searched via allMarkdownRemark)
+
+export const query = graphql`
+  {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          title
+          image
+        }
+      }
+    }
+  }
+`
